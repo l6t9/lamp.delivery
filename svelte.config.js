4a67285e45c1397156226cddeb7b3bd95ba55301
l6t9 <158360149+l6t9@users.noreply.github.com>
2026-01-10 06:39:13 -0500

    hi

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import { mdsvex, escapeSvelte } from 'mdsvex';
-import { createHighlighter } from 'shiki';
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+import Icons from 'unplugin-icons/vite';
 
 
 /** @type {import('mdsvex').MdsvexOptions} */
@@ -31,10 +29,13 @@ const config = {
         // Consult https://svelte.dev/docs/kit/integrations
         // for more information about preprocessors
         preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-        kit: { // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-                // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-                // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-                adapter: adapter()
+        kit: {
+                adapter: adapter(),
+                vite: {
+                        plugins: [
+                                Icons({ compiler: 'svelte' })
+                        ]
+                }
         },
         extensions: ['.svelte', '.svx'],
 };
