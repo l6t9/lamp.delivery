d5fb1fb7d1adb841a203af796b82677857e5393c
l6t9 <158360149+l6t9@users.noreply.github.com>
2026-05-30 00:13:46 -0400

    Restore gitlink and remove unused configs

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_58isFfGV.mjs';
-import { manifest } from './manifest_IjaU4HK6.mjs';
+import { manifest } from './manifest_CCMzp5QC.mjs';
 
 const serverIslandMap = new Map();;
 
