8dd8ad0db75c629b94d0ea9864717103d29b2f83
l6t9 <158360149+l6t9@users.noreply.github.com>
2026-05-30 01:05:13 -0400

    fix mess

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,21 +10,5 @@ export default defineConfig({
   server: {
     host: true,
     port: 4321
-  },
-  vite: {
-    server: {
-      watch: {
-        ignored: [
-          '**/node_modules/**',
-          '**/.git/**',
-          '**/.opencode/**',
-          '**/.wrangler/**',
-          '**/.astro/**',
-          '**/dist/**',
-          '**/.idea/**',
-          '**/downloads/**'
-        ]
-      }
-    }
   }
 });
